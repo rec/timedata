84541bea51a2f8f4971426b38255edfaba70d7a7
Tom Ritchford <tom@swirly.com>
2016-08-10 15:37:58 -0400

    inc

--- src/cpp/timedata/color/renderer_inl.h
+++ src/cpp/timedata/color/renderer_inl.h
@@ -19,7 +19,7 @@ template <typename Colors>
 void CRenderer::renderGeneric(
         float level, Colors const& colors, size_t size, char* out) {
     for (size_t i = 0; i < size; ++i) {
-        auto color = colors[i];
+        auto color = colors(i);
         for (size_t j = 0; j < color.size(); ++j, ++out) {
             auto component = level * color[perm_[j]];
             auto gamma = getGamma(gammaTable_, component);
@@ -30,8 +30,7 @@ void CRenderer::renderGeneric(
 
 inline void CRenderer::render(
         float level, CColorListRGB const& colors, char* out) {
-    return renderGeneric(
-        level, RGBAdaptor<CColorListRGB>(colors), colors.size(), out);
+    return renderGeneric(level, getIndexer(colors), colors.size(), out);
 }
 
 inline CRenderer::Perm CRenderer::getPerm(Render3::Permutation perm) {
--- src/cpp/timedata/color/rgbAdaptor.h
+++ src/cpp/timedata/color/rgbAdaptor.h
@@ -9,7 +9,6 @@ namespace color_list {
 
 using RGBIndexer = std::function<ColorRGB (size_t index)>;
 
-
 template <typename ColorList>
 RGBIndexer getIndexer(ColorList const& colors) {
     return [&] (size_t i) {
